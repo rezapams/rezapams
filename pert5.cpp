61b481e7aa3d7c78d219714047c7432be53cdf99
reza <129819582+rezapams@users.noreply.github.com>
2023-06-04 22:28:44 +0700

    Add files via upload

---
+++ pert5.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+ int a=20;
+ int b=10;
+
+ cout <<"contoh if  else statment"<<endl;
+
+ if(a>b){
+ cout<<"lebih besaar dari B"<<endl;
+
+ }
+ else{
+    cout<<"b lebih besar a"<<endl;
+ }
+ return 0;
+
+}
